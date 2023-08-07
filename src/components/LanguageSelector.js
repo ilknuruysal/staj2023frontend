90e0be3e07f8c5bcab1c4cace2f8489fd880cecd
Semir Tatli <semirtatli@outlook.com>
2023-08-07 15:52:17 +0300

    Login Page ve Sign Up ortak dil destegi eklendi

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -12,12 +12,13 @@ const LanguageSelector = props => {
   return (
     <div className="container">
       <img
-        src="https://www.countryflags.io/tr/flat/24.png"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/800px-Flag_of_Turkey.svg.png"
         alt="Turkish Flag"
+        height={100}
         onClick={() => onChangeLanguage('tr')}
         style={{ cursor: 'pointer' }}
       ></img>
-      <img src="https://www.countryflags.io/us/flat/24.png" alt="USA Flag" onClick={() => onChangeLanguage('en')} style={{ cursor: 'pointer' }}></img>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/US_flag_51_stars.svg/2560px-US_flag_51_stars.svg.png" alt="USA Flag" height={100} onClick={() => onChangeLanguage('en')} style={{ cursor: 'pointer' }}></img>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import './bootstrap-override.scss';
 import App from './App';
 import UserSignupPage from './pages/UserSignupPage';
-import UserLoginPage from './pages/UserLoginPage';
+import LoginPage from './pages/LoginPage';
 import * as serviceWorker from './serviceWorker';
 import './i18n';
 import LanguageSelector from './components/LanguageSelector';
 
 ReactDOM.render(
-<div>
-    <UserLoginPage />
-    <LanguageSelector />
-</div>, document.getElementById('root')
+    <div>
+        <UserSignupPage />
+        <LanguageSelector />
+    </div>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
