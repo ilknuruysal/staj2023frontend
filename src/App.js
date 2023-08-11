bdd3fcef4b2a95a719c09ad8f11ef82b54ef493c
Semir Tatli <semirtatli@outlook.com>
2023-08-11 11:55:26 +0300

    Product ekranı eklendi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Col, Container, Row } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> is changed second time.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { currentCategory: "", products: [], cart:[] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+
+    this.getProducts(category.id);
+  };
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if(categoryId){
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
+  addToCart=(product)=>{
+    
+    let newCart = this.state.cart;
+    var addedItem = newCart.find(c=>c.product.id ===product.id);
+    if(addedItem){
+      addedItem.quantity+=1;
+    } else {
+      newCart.push({product:product, quantity:1});
+    }
+    
+    this.setState({cart:newCart});
+  }
+
+  render() {
+    let categoryInfo = { title: "Category List" };
+    let productInfo = { title: "Product List" };
+    return (
+      <div>
+        <Container>
+
+            <Navi cart={this.state.cart}/>
+          
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                products={this.state.products}
+                addToCart= {this.addToCart}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Sign Up Sign In Page
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -8,16 +9,27 @@ import LoginPage from "./pages/LoginPage";
 import * as serviceWorker from "./serviceWorker";
 import "./i18n";
 import LanguageSelector from "./components/LanguageSelector";
+// import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <div>
-    <LoginPage />
-    <LanguageSelector />
-  </div>,
-  document.getElementById("root")
+// ReactDOM.render(
+//   <div>
+//     <UserSignupPage />
+//     <LanguageSelector />
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// serviceWorker.unregister();
+
+//The product page
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals();
